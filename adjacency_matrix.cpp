424d5d5635f1fc3bdd46ee3a3e823494b1f828e9
JJoaoll <joaoduos@gmail.com>
2026-03-21 15:41:29 -0300

    add: adjacency_matrix
    Just a very very simple implementation in a oop-style (🤮)

---
+++ adjacency_matrix.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+
+template <size_t N>
+class AdjacencyMatrix {
+private: 
+  std::array<std::array<int, N>, N> edges{};
+public: 
+  template <typename Iter>
+  static AdjacencyMatrix mkDirected (Iter begin, Iter end) {
+    AdjacencyMatrix graph;
+    for (auto it = begin; it != end; ++it) {
+      graph->edges[it->first][it->second] = 1;
+    }
+  }
+
+  template <typename Iter>
+  static AdjacencyMatrix mkNDirected(Iter begin, Iter end) {
+    AdjacencyMatrix graph;
+    for (auto it = begin; it != end; ++it) {
+      graph->edges[it->first][it->second] = 1;
+      graph->edges[it->second][it->first] = 1;
+    }
+  }
+};
+
+
