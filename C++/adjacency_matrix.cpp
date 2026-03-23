1978e60ab5023e20a957cc383de1420adefc401d
schwaad <joseivoschwadea@gmail.com>
2026-03-23 18:06:40 -0300

    fix: removed unused imports

--- C++/adjacency_matrix.cpp
+++ C++/adjacency_matrix.cpp
@@ -1,4 +1,4 @@
-#include <vector>
+#include "graph.h"
 #include <iostream>
 #include <map>
 #include <algorithm>
@@ -12,54 +12,53 @@ class AdjacencyMatrix : public Graph{
         int numVertices;
         bool isDirected;
 
-    public: 
-
-        AdjacencyMatrix( int numVertices, bool directed = false) : isDirected(directed) {
-            this->numVertices = numVertices;
-            matrix = new int*[numVertices];
-            for (int i = 0; i < numVertices; i++){
-                matrix[i]= new int[numVertices];
-                for (int j = 0; j<numVertices; j++){
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-
-        void addEdge(int from, int to){
-            matrix[from][to] = 1;
-
-            if (!isDirected){
-                matrix[to][from] = 1;
-            }
-        }
-
-        void removeEdge(int from, int to){
-            matrix[from][to] = 0;
-
-            if (!isDirected){
-                matrix[to][from] = 0;
-            }
-        }
-
-        void printMatrix() {
-            for (int i = 0; i < numVertices; i++) {
-            cout << i << " : ";
-            for (int j = 0; j < numVertices; j++)
-                cout << matrix[i][j] << " ";
-            cout << "\n";
-            }
-        }
-
-    vector<int> getNeighbors(int node) {
-        vector<int> tempNeighbors;
-        
-        for (int i = 0; i < numVertices; i++) {
-            if (matrix[node][i] == 1) {
-                tempNeighbors.push_back(i);
-            }
-        }
-        
-        return tempNeighbors;
+public:
+  AdjacencyMatrix(int numVertices, bool directed = false)
+      : isDirected(directed) {
+    this->numVertices = numVertices;
+    matrix = new int *[numVertices];
+    for (int i = 0; i < numVertices; i++) {
+      matrix[i] = new int[numVertices];
+      for (int j = 0; j < numVertices; j++) {
+        matrix[i][j] = 0;
+      }
     }
-};
+  }
 
+  void addEdge(int from, int to) {
+    matrix[from][to] = 1;
+
+    if (!isDirected) {
+      matrix[to][from] = 1;
+    }
+  }
+
+  void removeEdge(int from, int to) {
+    matrix[from][to] = 0;
+
+    if (!isDirected) {
+      matrix[to][from] = 0;
+    }
+  }
+
+  void printMatrix() {
+    for (int i = 0; i < numVertices; i++) {
+      cout << i << " : ";
+      for (int j = 0; j < numVertices; j++)
+        cout << matrix[i][j] << " ";
+      cout << "\n";
+    }
+  }
+
+  vector<int> getNeighbors(int node) {
+    vector<int> tempNeighbors;
+
+    for (int i = 0; i < numVertices; i++) {
+      if (matrix[node][i] == 1) {
+        tempNeighbors.push_back(i);
+      }
+    }
+
+    return tempNeighbors;
+  }
+};
