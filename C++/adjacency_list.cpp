2ab7fd2ed603bbbcae83d1bed730f7674f906df3
André Gomes <andrelucasrngomes@gmail.com>
2026-03-23 09:19:09 -0300

    add: adjacency list graph implemented with map

---
+++ C++/adjacency_list.cpp
@@ -0,0 +1,60 @@
+#include <vector>
+#include <iostream>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+class AdjacencyList {
+private: 
+  map<int, vector<int>> adj;
+  bool isDirected;
+
+public: 
+
+  AdjacencyList(bool directed = false) : isDirected(directed) {
+  }
+
+  void addVertex(int node) {
+    // Ensure non-duplicate vertices
+    // int node will be vertex's label
+    if (adj.find(node) == adj.end()) {
+      adj[node] = vector<int>();
+    }
+  }
+
+  void addEdge(int from, int to) {
+    // Ensure vertices exist before adding edges
+    addVertex(from);
+    addVertex(to);
+    adj[from].push_back(to);
+    if (!isDirected) {
+      adj[to].push_back(from);
+    }
+  }
+
+  void removeVertex(int node){
+    // Remove vertex and all associated edges
+    adj.erase(node);
+    for (auto& [key, neighbors] : adj) {
+      neighbors.erase(remove(neighbors.begin(), neighbors.end(), node), neighbors.end());
+    }
+  }
+
+  void printGraph() {
+    for (const auto& [node, neighbors] : adj) {
+      cout << node << ": ";
+      for (int neighbor : neighbors) {
+        cout << neighbor << " ";
+      }
+      cout << endl;
+    }
+  }
+
+  vector<int> getNeighbors(int node){
+    return adj[node];
+  }
+
+};
+
+
