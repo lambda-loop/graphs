21263fdcad985d5bdb758c46d6443a6d3501fafe
JJoaoll <joaoduos@gmail.com>
2026-04-19 14:55:03 -0300

    DFS before AI

--- Trabalho1/DFS.h
+++ Trabalho1/DFS.h
@@ -8,6 +8,8 @@
 #include <optional>
 #include <set>
 #include <stack>
+#include <map>
+#include <vector>
 
 class DFS {
 public:
@@ -24,14 +26,14 @@ public:
     this->return_edges = {};
   }
 
-  DFS (ListaDeAdjacencia<int> g, int P0) {
+  DFS (ListaDeAdjacencia<int> &g, int VAL) {
     // a fila do slide
     std::stack<int> stack;
-    stack.push(P0); // fazendo uso da P0
+    stack.push(VAL); // fazendo uso da P0
     
     // o array dos visitados do slide
     std::set<int> visited;
-    visited.insert(P0);
+    visited.insert(VAL);
 
     // pq o grupo usou MAP, eu tenho q fazer assim. 
     // triste estou 😭 
@@ -41,7 +43,7 @@ public:
     std::map<int, std::vector<int>::iterator> iters;
 
     // muito importante pra não bugar..
-    iters[P0] = g.adjacentes[P0].begin();
+    iters[VAL] = g.adjacentes[VAL].begin();
 
     std::map<int, int> PES;
     std::map<int, int> PSS;
@@ -49,29 +51,35 @@ public:
     int PS_counter = 0;
     
     // A estrutura intermediária
-    DFS dfs = DFS(P0);
+    // DFS dfs = DFS(P0);
 
     // LOUCURA:
     std::map<int, DFS*> refs;
     // std::map<int, std::shared_ptr<DFS>> refs; 
     // refs[P0]        = std::make_shared<DFS>(dfs);
-    // this->val = P0;
-    // refs[P0] = this;
-    // predecessor[P0] = std::nullopt;
+    this->val = VAL;
+    refs[VAL] = this;
+    predecessor[VAL] = std::nullopt;
 
     // e vamos pro algoritmo.
     while(!stack.empty()) {
       int P0 = stack.top();
+      std::cout << "nó atual: " << P0 << std::endl;
       
+      std::cout << "Tem nó pra visitar?" << std::endl;
       // tem nós pra visitar?
       if (iters[P0] != g.adjacentes[P0].end()) {
+        std::cout << "Tem" << std::endl;
         // visitando o..
         int P = *iters[P0];
 
         // se já foi visitado // TODO: devia ser o iters do P
+        
+        std::cout << "Ja foi visitado" << std::endl;
         if (visited.find(P) != visited.end()) { 
           // aresta de retorno?
           if (predecessor[P0] != P) {
+            std::cout << "Eh aresta de retorno" << std::endl;
             // note que P já foi visitado..
             refs[P0]->return_edges.insert(refs[P]);
           }
@@ -84,7 +92,9 @@ public:
 
         // se ainda não foi visitado
         else {
+          std::cout << "Empilha o " << P << std::endl;
           stack.push(P);
+          std::cout << "Marca o " << P << " como visitado" << std::endl;
           visited.insert(P);
           iters[P] = g.adjacentes[P].begin();
 
@@ -108,9 +118,15 @@ public:
 
       // caso ja foi visitado, desempilha!
       else {
+
+        std::cout << "Tem não." << std::endl;
         // current vira o pai dele após o pop
+        std::cout << "Desempilha " << P0                 <<  std::endl;
+        std::cout << "PS de " << P0 << ": " << PS_counter << std::endl;
+
         stack.pop();
         PSS[P0] = PS_counter;
+
         PS_counter++;
       }
     }
