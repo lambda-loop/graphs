9ffb16a35ea4b7cf15afd7669c26d7b347b1bd4c
JJoaoll <joaoduos@gmail.com>
2026-04-24 17:12:18 -0300

    👀

--- Trabalho1/questao3.cpp
+++ Trabalho1/questao3.cpp
@@ -3,15 +3,12 @@
 int main() {
   GrafoMatrizInc g(8);
 
-  g.adicionarAresta('a', 'b');
-  g.adicionarAresta('b', 'c');
-  g.adicionarAresta('b', 'd');
-  g.adicionarAresta('c', 'd');
-  g.adicionarAresta('c', 'e');
-  g.adicionarAresta('e', 'f');
-  g.adicionarAresta('f', 'g');
-  g.adicionarAresta('f', 'h');
-  g.adicionarAresta('g', 'h');
+  g.adicionarAresta(1, 2);
+  g.adicionarAresta(1, 2);
+  g.adicionarAresta(1, 2);
+  g.adicionarAresta(1, 2);
+  g.adicionarAresta(1, 2);
+
 
   g.imprimirMatriz();
 
