e528c4edf824ea9ec5bb498cde6754d766fb91b0
João Pereira <joaoduosl@gmail.com>
2026-05-21 14:30:05 -0300

    continua ruim.

--- UII/graph.zig
+++ UII/graph.zig
@@ -1,9 +1,9 @@
 const std = @import("std");
 
-pub const PairIntFloat = struct {
-    x: u32,
-    y: f32,
-};
+// pub const PairIntFloat = struct {
+//     x: u32,
+//     y: f32,
+// };
 
 pub const Edge = struct {
     from: u32,
@@ -13,44 +13,44 @@ pub const Edge = struct {
 
 // only integers with floats as weight for a while..
 pub const Graph = struct {
-    data: std.AutoHashMap(u32, std.ArrayList(PairIntFloat)),
+    data: std.AutoHashMap(u32, std.ArrayList(Edge)),
     is_directed: bool,
     allocator: std.mem.Allocator,
 
     pub fn init(allocator: std.mem.Allocator, is_directed: bool, triples: []const Edge) !Graph {
         var graph = Graph{
-            .data = std.AutoHashMap(u32, PairIntFloat).init(allocator),
+            .data = std.AutoHashMap(u32, 
+                std.ArrayList(Edge)).init(allocator),
             .is_directed = is_directed,
-            .allocatgor = allocator,
+            .allocator = allocator,
         };
 
         errdefer graph.data.deinit();
         for (triples) |edge| {
-            try graph.addEdge(edge.from, edge.to, edge.weight);
-            // try graph.data.put(edge.from, .{ .x = edge.to, .y = edge.weight });
+            try graph.addEdge(edge);
 
             if (!is_directed) {
-                try graph.addEdge(edge.to, edge.from, edge.weight);
+                try graph.addEdge(.{.from = edge.to, .weight = edge.weight, .to = edge.from});
             }
         }
 
         return graph;
     }
 
-    fn addEdge(self: *Graph, from: u32, to: u32, weight: f32) !void {
-        const res = try self.data.getOrPut(from);
+    fn addEdge(self: *Graph, edge: Edge) !void {
+        const res = try self.data.getOrPut(edge.from);
 
         if (!res.found_existing) {
-            res.value_ptr.* = std.ArrayList(PairIntFloat).init(self.allocator);
+            res.value_ptr.* = std.ArrayList(Edge).init(self.allocator);
         }
 
-        try res.value_ptr.append(.{ .x = to, .y = weight });
+        try res.value_ptr.append(edge);
     }
 
     // u pick the first vertice. Whatever..
     pub fn prim(self: Graph, V: u32) Graph {
-        var Z = std.AutoHashMap(i32, void).init(self.allocator);
-        defer self.deinit();
+        var Z = std.AutoArrayHashMap(u32, void).init(self.allocator);
+        defer Z.deinit();
 
         Z.put(V, {});
 
@@ -59,13 +59,13 @@ pub const Graph = struct {
         const NUM_KEYS = self.data.count();
         while (Z.count() < NUM_KEYS) {
             // const KEYS = std.AutoArrayHashMap(i32, void).init(self.allocator);
-            const MIN_VERTICE = find_min: {
+            const MIN_VERTICE: Edge = find_min: {
                 var min = std.math.inf(f32);
-                var min_orange = undefined;
-                for (Z.keys()) |green| {
-                    for (self.data.get(green)) |orange| {
-                        if (!Z.contains(orange.x) and orange.y < min) { 
-                            min = orange.y; 
+                var min_orange: Edge = undefined;
+                for (Z.keys()) |green| { // greens are the visited vertexes
+                    for (self.data.get(green)) |orange| { // oranges are the edges
+                        if (!Z.contains(orange.to) and orange.weigth < min) { 
+                            min = orange.weigth; 
                             min_orange = orange;
                         }
                     }
